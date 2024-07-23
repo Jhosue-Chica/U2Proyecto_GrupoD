df9b6ff155f147f89b7acb3cd599b57049271436
Jhosue-Chica <jichica@espe.edu.ec>
2024-07-23 12:53:47 -0500

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+})
+export class AppModule {
+  ngDoBootstrap() {
+    // bootstrap the application
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
+import { ExploreComponent } from './explore/explore.component';
+import { SongsComponent } from './songs/songs.component';
+import { EventsComponent } from './events/events.component';
+import { AccountComponent } from './account/account.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'store', component: StoreComponent },
+    { path: 'explore', component: ExploreComponent },
+    { path: 'songs', component: SongsComponent },
+    { path: 'events', component: EventsComponent },
+    { path: 'account', component: AccountComponent },
+    { path: 'about', component: AboutComponent }
+];
