18f0979992e504bec40c8ee796e25ddf11249d08
raulfaz <refaz@espe.edu.ec>
2024-09-04 08:35:11 -0500

    store 1.2

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -4,12 +4,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Product, PRODUCTS } from '../store/product-data'; // Importa los datos directamente
 import { CommonModule } from '@angular/common';
+import { CartService } from '../cart.service';
+import { CartComponent } from '../cart/cart.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-store',
   standalone: true,
-  imports: [MatCardModule,MatButtonModule,MatIconModule,CommonModule],
+  imports: [MatCardModule,MatButtonModule,MatIconModule,CommonModule,CartComponent,RouterModule],
   templateUrl: './store.component.html',
   styleUrl: './store.component.css'
 })
@@ -19,5 +22,11 @@ export class StoreComponent {
 
   url: string = 'https://api.whatsapp.com/send?phone=593986439931&text=Hola%20deseo%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20servicios%20de%20vivero%F0%9F%98%8A';
 
+  constructor(private cartService: CartService) {}
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    alert(`${product.name} ha sido añadido al carrito!`);
+  }
  
 }
